0a47beed41006c249118001ade573ac42e754cd2
Shubham <shubhamk604@gmail.com>
2021-03-11 21:24:34 +0530

    commiting 1st commit

---
+++ todos.rb
@@ -0,0 +1,8 @@
+#1st ruby comment
+puts "what is yout next-todo"
+next_todo = gets
+puts " these ur next to-do: "
+puts next_todo
+puts " \n1st one is #{next_todo} will plan futher later"
+puts " \n #{4+3}"
+puts " just a random line"
