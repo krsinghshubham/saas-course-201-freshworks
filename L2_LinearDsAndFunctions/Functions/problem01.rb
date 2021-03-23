b3ec47007c226852ad1786a024eb577c83b031a3
shubham <shubhamk604@gmail.com>
2021-03-24 01:54:47 +0530

    auto committ

--- L2_LinearDsAndFunctions/Functions/problem01.rb
+++ L2_LinearDsAndFunctions/Functions/problem01.rb
@@ -1,13 +1,6 @@
-# Implement a todo list using classes
-# For this milestone target, your program must output the following:
-# My Todo-list
-# Overdue
-# [ ] Submit assignment 2020-01-21
-# Due Today
-# [X] Pay rent
-# [ ] Service vehicle
-# Due Later
-# [ ] File taxes 2020-01-23
-# [ ] Call Acme Corp. 2020-01-23
-# Please note that the dates will vary based on the current date, and its values are supplied in the template below.
-# Template is passed to questions.rb
+def salute(name, salutation)
+  "#{salutation.capitalize} Mr. #{name.split(" ").last}"
+end
+
+puts salute("Nelson Rolihlahla Mandela", "hello")
+puts salute("Sir Alan Turing", "welcome")
---
+++ L3-Classes/problem01.rb
@@ -0,0 +1,13 @@
+# Implement a todo list using classes
+# For this milestone target, your program must output the following:
+# My Todo-list
+# Overdue
+# [ ] Submit assignment 2020-01-21
+# Due Today
+# [X] Pay rent
+# [ ] Service vehicle
+# Due Later
+# [ ] File taxes 2020-01-23
+# [ ] Call Acme Corp. 2020-01-23
+# Please note that the dates will vary based on the current date, and its values are supplied in the template below.
+# Template is passed to questions.rb
